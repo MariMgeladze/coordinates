48e977745832f3d1e1edec6e727d965e4596eaf4
MariMgeladze <mgeladze17@gmail.com>
2023-11-01 20:09:50 +0400

    remove extra files

--- coordinates/src/main/java/softlab/homework/coordinates/repositories/ExcelInfoRepository.java
+++
@@ -1,8 +0,0 @@
-package softlab.homework.coordinates.repositories;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-import softlab.homework.coordinates.entities.ExcelInfo;
-
-public interface ExcelInfoRepository extends JpaRepository<ExcelInfo, Integer> {
-}
